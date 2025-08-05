692f395fcc45c8a8c3e0be45bfe865365591382c
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-08-06 00:14:25 +0800

    Create Page prop 'page-props'

---
+++ resources/js/types/page-props.ts
@@ -0,0 +1,15 @@
+import { Transfer } from './transfer';
+import { InventoryList } from './inventory-list';
+import { Building } from './building';
+import { BuildingRoom } from './building-room';
+import { UnitOrDepartment } from './unit-or-department';
+import { User } from './user';
+
+export type TransferPageProps = {
+    transfers: Transfer[];
+    assets: InventoryList[];
+    buildings: Building[];
+    buildingRooms: BuildingRoom[];
+    unitOrDepartments: UnitOrDepartment[];
+    users: User[];
+}
