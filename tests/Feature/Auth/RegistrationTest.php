844cd24774ed8146a86dec8a8731f01a9e3a33f9
rikuu-nifty <venaljansen30@yahoo.com>
2025-08-10 20:25:02 +0800

    Implementing OTP Features

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -26,6 +26,7 @@ class RegistrationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertRedirect(route('otp.notice', absolute: false));
+
     }
 }
