7066cc0564a5a23c9d8b06e92b9e0cae7640c4f9
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-08-29 18:06:27 +0800

    Defined gates for role assignment, role reassignment, and users page viewing

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use Inertia\Inertia;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        // User::class => UserPolicy::class,
+    ];
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Gate::define('view-users-page', function (User $user) {
+            return in_array($user->role?->code, ['vp_admin', 'pmo_head']);
+        });
+
+        Gate::define('assign-role', function (User $user, string $targetRoleCode) {
+
+            if ($user->role?->code === 'vp_admin') {
+                return true;
+            }
+
+            if ($user->role?->code === 'pmo_head') {
+                return in_array($targetRoleCode, ['pmo_staff']);
+            }
+
+            return false; // default deny
+        });
+
+        Gate::define('reassign-role', function (User $user, string $targetRoleCode) {
+            return Gate::forUser($user)->allows('assign-role', $targetRoleCode);
+        });
+    }
+}
