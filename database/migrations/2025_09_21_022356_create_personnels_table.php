29c870f6f570ff2a284248b2a1cc97efe0c25062
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-09-21 03:05:17 +0800

    Migration file for Personnels in Charge

---
+++ database/migrations/2025_09_21_022356_create_personnels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personnels', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+
+            $table->string('position')->nullable();
+            $table->unsignedBigInteger('unit_or_department_id')->nullable(); //just in case for the view own unit permission
+            $table->enum('status', ['active', 'inactive', 'left_university'])->default('active'); //inactive for on leave and left_university
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('unit_or_department_id')->references('id')->on('unit_or_departments')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personnels');
+    }
+};
