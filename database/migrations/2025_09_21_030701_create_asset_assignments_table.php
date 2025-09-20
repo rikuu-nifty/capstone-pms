e64303d5b6d75c185303a7a4d15273441c46e5f3
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-09-21 03:17:04 +0800

    Migration file for Asset Assignment

---
+++ database/migrations/2025_09_21_030701_create_asset_assignments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asset_assignments', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('asset_id');
+            $table->unsignedBigInteger('unit_or_department_id');
+            $table->unsignedBigInteger('personnel_id');
+
+            $table->unsignedBigInteger('assigned_by')->nullable(); //nullable just in case of historical imports
+            $table->date('date_assigned');
+            $table->text('remarks')->nullable();
+
+            $table->timestamps();
+            //no soft deletes for audit trail
+
+            $table->foreign('asset_id')->references('id')->on('inventory_lists')->cascadeOnDelete();
+            $table->foreign('unit_or_department_id')->references('id')->on('unit_or_departments')->cascadeOnDelete();
+            $table->foreign('personnel_id')->references('id')->on('personnels')->cascadeOnDelete();
+            $table->foreign('assigned_by')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asset_assignments');
+    }
+};
