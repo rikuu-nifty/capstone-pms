32f28401eec5e754992c1f30a6df1f55652fcf01
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-10-04 05:00:21 +0800

    Checkpoint

--- app/Http/Controllers/TrashBinController.php
+++ app/Http/Controllers/TrashBinController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+
 use App\Models\InventoryList;
 use App\Models\InventoryScheduling;
 use App\Models\Transfer;
 use App\Models\TurnoverDisposal;
 use App\Models\OffCampus;
+use App\Models\AssetModel;
+use App\Models\Category;
+use App\Models\AssetAssignment;
+use App\Models\EquipmentCode;
 
 class TrashBinController extends Controller
 {
@@ -45,11 +50,17 @@ class TrashBinController extends Controller
         };
 
         return Inertia::render('trash-bin/index', [
-            'inventory_lists'      => $applyFilters(InventoryList::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'inventory_lists'       => $applyFilters(InventoryList::onlyTrashed())->paginate($perPage)->withQueryString(),
             'inventory_schedulings' => $applyFilters(InventoryScheduling::onlyTrashed())->paginate($perPage)->withQueryString(),
-            'transfers'            => $applyFilters(Transfer::onlyTrashed())->paginate($perPage)->withQueryString(),
-            'turnover_disposals'   => $applyFilters(TurnoverDisposal::onlyTrashed())->paginate($perPage)->withQueryString(),
-            'off_campuses'         => $applyFilters(OffCampus::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'transfers'             => $applyFilters(Transfer::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'turnover_disposals'    => $applyFilters(TurnoverDisposal::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'off_campuses'          => $applyFilters(OffCampus::onlyTrashed())->paginate($perPage)->withQueryString(),
+
+            // Assets group
+            'asset_models'          => $applyFilters(AssetModel::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'categories'            => $applyFilters(Category::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'assignments'           => $applyFilters(AssetAssignment::onlyTrashed())->paginate($perPage)->withQueryString(),
+            'equipment_codes'       => $applyFilters(EquipmentCode::onlyTrashed())->paginate($perPage)->withQueryString(),
 
             'filters' => [
                 'date_filter' => $filter,
@@ -76,6 +87,12 @@ class TrashBinController extends Controller
             'transfer'           => Transfer::class,
             'turnover-disposal'  => TurnoverDisposal::class,
             'off-campus'         => OffCampus::class,
+
+            'asset-model'        => AssetModel::class,
+            'category'           => Category::class,
+            'assignment'         => AssetAssignment::class,
+            'equipment-code'     => EquipmentCode::class,
+
             default => abort(404),
         };
     }
