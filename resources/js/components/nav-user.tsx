18e87bcabfaa2b028209760a82cf908c52ba4647
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-10-11 16:41:22 +0800

    Cursor Pointer

--- resources/js/components/nav-user.tsx
+++ resources/js/components/nav-user.tsx
@@ -17,7 +17,7 @@ export function NavUser() {
             <SidebarMenuItem>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton size="lg" className="group text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent">
+                        <SidebarMenuButton size="lg" className="group text-sidebar-accent-foreground data-[state=open]:bg-sidebar-accent cursor-pointer hover:bg-blue-200">
                             <UserInfo user={auth.user} />
                             <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
