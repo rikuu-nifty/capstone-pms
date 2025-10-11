8312efa08dc13b72d8d2cb5f3cd269d688eadfdb
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-10-11 11:15:55 +0800

    Roles and Permissions Seeder: Adjusted perms to follow sidebar order

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -10,43 +10,6 @@ class PermissionsTableSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-            // USERS MANAGEMENT PAGE
-            ['code' => 'view-users-page', 'name' => 'View Users Page'],
-            ['code' => 'approve-users', 'name' => 'Approve Users'],
-            ['code' => 'reset-user-password', 'name' => 'Reset User Password'],
-            ['code' => 'send-email-change-request', 'name' => 'Send Email Change Request'],
-            ['code' => 'delete-users', 'name' => 'Delete Users'],
-            
-            // ROLES & PERMISSIONS
-            ['code' => 'view-roles-page', 'name' => 'View Roles Page'],
-            ['code' => 'create-roles', 'name' => 'Create Roles'],
-            ['code' => 'update-roles', 'name' => 'Update Roles'],
-            ['code' => 'delete-role', 'name' => 'Delete Roles'],
-            ['code' => 'update-permissions', 'name' => 'Update Permissions'],
-
-            // TRASH BIN
-            ['code' => 'view-trash-bin', 'name' => 'View Trash Bin'],
-            ['code' => 'restore-trash-bin', 'name' => 'Restore Trash Bin'],
-            ['code' => 'permanent-delete-trash-bin', 'name' => 'Permanently Delete Trash Bin'],
-
-            // NOTIFICATIONS
-            ['code' => 'view-notifications', 'name' => 'View Notifications'],
-            ['code' => 'update-notifications', 'name' => 'Update Notifications'],
-            ['code' => 'archive-notifications', 'name' => 'Archive Notifications'],
-            ['code' => 'delete-notifications', 'name' => 'Delete Notifications'],
-
-            // SIGNATORIES
-            ['code' => 'view-signatories', 'name' => 'View Signatories'],
-            ['code' => 'create-signatories', 'name' => 'Create Signatories'],
-            ['code' => 'update-signatories', 'name' => 'Update Signatories'],
-            ['code' => 'delete-signatories', 'name' => 'Delete Signatories'],
-
-            // INVENTORY SCHEDULING
-            ['code' => 'view-inventory-scheduling', 'name' => 'View Inventory Scheduling'],
-            ['code' => 'create-inventory-scheduling', 'name' => 'Create Inventory Scheduling'],
-            ['code' => 'update-inventory-scheduling', 'name' => 'Update Inventory Scheduling'],
-            ['code' => 'delete-inventory-scheduling', 'name' => 'Delete Inventory Scheduling'],
-
             // INVENTORY LIST
             ['code' => 'view-inventory-list', 'name' => 'View Inventory List'],
             ['code' => 'view-own-unit-inventory-list', 'name' => 'View Own Unit Inventory List'],
@@ -54,38 +17,31 @@ class PermissionsTableSeeder extends Seeder
             ['code' => 'update-inventory-list', 'name' => 'Update Inventory List'],
             ['code' => 'delete-inventory-list', 'name' => 'Delete Inventory List'],
             
+            // INVENTORY SCHEDULING
+            ['code' => 'view-inventory-scheduling', 'name' => 'View Inventory Scheduling'],
+            ['code' => 'create-inventory-scheduling', 'name' => 'Create Inventory Scheduling'],
+            ['code' => 'update-inventory-scheduling', 'name' => 'Update Inventory Scheduling'],
+            ['code' => 'delete-inventory-scheduling', 'name' => 'Delete Inventory Scheduling'],
+
             // TRANSFER
             ['code' => 'view-transfers', 'name' => 'View Transfers'],
             ['code' => 'create-transfers', 'name' => 'Create Transfers'],
             ['code' => 'update-transfers', 'name' => 'Update Transfers'],
             ['code' => 'delete-transfers', 'name' => 'Delete Transfers'],
-
+            
             // TURNOVER / DISPOSAL
             ['code' => 'view-turnover-disposal', 'name' => 'View Turnover/Disposal'],
             ['code' => 'create-turnover-disposal', 'name' => 'Create Turnover/Disposal'],
             ['code' => 'update-turnover-disposal', 'name' => 'Update Turnover/Disposal'],
             ['code' => 'delete-turnover-disposal', 'name' => 'Delete Turnover/Disposal'],
-
+            
             // OFF CAMPUS
             ['code' => 'view-off-campus', 'name' => 'View Off Campus'],
             ['code' => 'create-off-campus', 'name' => 'Create Off Campus'],
             ['code' => 'update-off-campus', 'name' => 'Update Off Campus'],
             ['code' => 'delete-off-campus', 'name' => 'Delete Off Campus'],
             ['code' => 'restore-off-campus', 'name' => 'Restore Off Campus'],
-
-            // BUILDINGS
-            ['code' => 'view-buildings', 'name' => 'View Buildings'],
-            ['code' => 'view-own-unit-buildings', 'name' => 'View Own Unit Buildings'],
-            ['code' => 'create-buildings', 'name' => 'Create Buildings'],
-            ['code' => 'update-buildings', 'name' => 'Update Buildings'],
-            ['code' => 'delete-buildings', 'name' => 'Delete Buildings'],
-
-            // BUILDING ROOMS
-            ['code' => 'view-building-rooms', 'name' => 'View Building Rooms'],
-            ['code' => 'create-building-rooms', 'name' => 'Create Building Rooms'],
-            ['code' => 'update-building-rooms', 'name' => 'Update Building Rooms'],
-            ['code' => 'delete-building-rooms', 'name' => 'Delete Building Rooms'],
-
+            
             // CATEGORIES
             ['code' => 'view-categories', 'name' => 'View Categories'],
             ['code' => 'create-categories', 'name' => 'Create Categories'],
@@ -103,35 +59,79 @@ class PermissionsTableSeeder extends Seeder
             ['code' => 'create-asset-models', 'name' => 'Create Asset Models'],
             ['code' => 'update-asset-models', 'name' => 'Update Asset Models'],
             ['code' => 'delete-asset-models', 'name' => 'Delete Asset Models'],
-
-            // UNIT OR DEPARTMENTS
-            ['code' => 'view-unit-or-departments', 'name' => 'View Unit or Departments'],
-            ['code' => 'create-unit-or-departments', 'name' => 'Create Unit or Departments'],
-            ['code' => 'update-unit-or-departments', 'name' => 'Update Unit or Departments'],
-            ['code' => 'delete-unit-or-departments', 'name' => 'Delete Unit or Departments'],
             
-            // FORM APPROVALS
-            ['code' => 'view-form-approvals', 'name' => 'View Form Approvals'],
-            ['code' => 'approve-form-approvals', 'name' => 'Approve Form Approvals'],
-            ['code' => 'delete-form-approvals', 'name' => 'Delete Form Approvals'],
-            
-            // REPORTS
-            ['code' => 'view-reports', 'name' => 'View Reports'],
-
-            // AUDIT LOG
-            ['code' => 'view-audit-logs', 'name' => 'View Audit Logs'],
-
             // ASSIGNMENTS
             ['code' => 'view-assignments', 'name' => 'View Assignments'],
             ['code' => 'create-assignments', 'name' => 'Create Assignments'],
             ['code' => 'update-assignments', 'name' => 'Update Assignments'],
             ['code' => 'delete-assignments', 'name' => 'Delete Assignments'],
+            
+            // BUILDINGS
+            ['code' => 'view-buildings', 'name' => 'View Buildings'],
+            ['code' => 'view-own-unit-buildings', 'name' => 'View Own Unit Buildings'],
+            ['code' => 'create-buildings', 'name' => 'Create Buildings'],
+            ['code' => 'update-buildings', 'name' => 'Update Buildings'],
+            ['code' => 'delete-buildings', 'name' => 'Delete Buildings'],
+
+            // BUILDING ROOMS
+            ['code' => 'view-building-rooms', 'name' => 'View Building Rooms'],
+            ['code' => 'create-building-rooms', 'name' => 'Create Building Rooms'],
+            ['code' => 'update-building-rooms', 'name' => 'Update Building Rooms'],
+            ['code' => 'delete-building-rooms', 'name' => 'Delete Building Rooms'],
 
             // PERSONNELS
             ['code' => 'view-personnels', 'name' => 'View Personnels'],
             ['code' => 'create-personnels', 'name' => 'Create Personnels'],
             ['code' => 'update-personnels', 'name' => 'Update Personnels'],
             ['code' => 'delete-personnels', 'name' => 'Delete Personnels'],
+            
+            // UNIT OR DEPARTMENTS
+            ['code' => 'view-unit-or-departments', 'name' => 'View Unit or Departments'],
+            ['code' => 'create-unit-or-departments', 'name' => 'Create Unit or Departments'],
+            ['code' => 'update-unit-or-departments', 'name' => 'Update Unit or Departments'],
+            ['code' => 'delete-unit-or-departments', 'name' => 'Delete Unit or Departments'],
+            
+            // USERS MANAGEMENT PAGE
+            ['code' => 'view-users-page', 'name' => 'View Users Page'],
+            ['code' => 'approve-users', 'name' => 'Approve Users'],
+            ['code' => 'reset-user-password', 'name' => 'Reset User Password'],
+            ['code' => 'send-email-change-request', 'name' => 'Send Email Change Request'],
+            ['code' => 'delete-users', 'name' => 'Delete Users'],
+            
+            // ROLES & PERMISSIONS
+            ['code' => 'view-roles-page', 'name' => 'View Roles Page'],
+            ['code' => 'create-roles', 'name' => 'Create Roles'],
+            ['code' => 'update-roles', 'name' => 'Update Roles'],
+            ['code' => 'delete-role', 'name' => 'Delete Roles'],
+            ['code' => 'update-permissions', 'name' => 'Update Permissions'],
+
+            // FORM APPROVALS
+            ['code' => 'view-form-approvals', 'name' => 'View Form Approvals'],
+            ['code' => 'approve-form-approvals', 'name' => 'Approve Form Approvals'],
+            ['code' => 'delete-form-approvals', 'name' => 'Delete Form Approvals'],
+            
+            // SIGNATORIES
+            ['code' => 'view-signatories', 'name' => 'View Signatories'],
+            ['code' => 'create-signatories', 'name' => 'Create Signatories'],
+            ['code' => 'update-signatories', 'name' => 'Update Signatories'],
+            ['code' => 'delete-signatories', 'name' => 'Delete Signatories'],
+
+            // NOTIFICATIONS
+            ['code' => 'view-notifications', 'name' => 'View Notifications'],
+            ['code' => 'update-notifications', 'name' => 'Update Notifications'],
+            ['code' => 'archive-notifications', 'name' => 'Archive Notifications'],
+            ['code' => 'delete-notifications', 'name' => 'Delete Notifications'],
+            
+            // REPORTS
+            ['code' => 'view-reports', 'name' => 'View Reports'],
+            
+            // TRASH BIN
+            ['code' => 'view-trash-bin', 'name' => 'View Trash Bin'],
+            ['code' => 'restore-trash-bin', 'name' => 'Restore Trash Bin'],
+            ['code' => 'permanent-delete-trash-bin', 'name' => 'Permanently Delete Trash Bin'],
+
+            // AUDIT LOG
+            ['code' => 'view-audit-logs', 'name' => 'View Audit Logs'],
                     
             // PROFILES
             ['code' => 'view-profile', 'name' => 'View Profile'],
--- database/seeders/RoleHasPermissionsTableSeeder.php
+++ database/seeders/RoleHasPermissionsTableSeeder.php
@@ -14,179 +14,291 @@ class RoleHasPermissionsTableSeeder extends Seeder
             'superuser' => Permission::pluck('id')->toArray(),
 
             'vp_admin' => [
-                'view-inventory-scheduling',
                 'view-inventory-list',
+                'view-inventory-scheduling',
                 'view-transfers',
                 'view-turnover-disposal',
                 'view-off-campus',
-                'view-reports',
+
+                'view-categories',
+                'view-equipment-codes',
+                'view-asset-models',
                 'view-assignments',
+                
                 'view-buildings',
                 'view-building-rooms',
-                'view-asset-models',
+                'view-personnels',
                 'view-unit-or-departments',
-                'view-own-unit-inventory-list',
 
+                // USERS
                 'view-users-page',
                 'approve-users',
                 'reset-user-password',
                 'send-email-change-request',
                 'delete-users',
-
+                
+                // ROLE MANAGEMENT
                 'view-roles-page',
                 'create-roles',
                 'update-roles',
                 'delete-role',
                 'update-permissions',
-
+                
+                // FORM APPROVAL
                 'view-form-approvals',
                 'approve-form-approvals',
                 'delete-form-approvals',
+                
+                'view-signatories',
 
+                'view-notifications',
+                'view-reports',
+                'view-trash-bin', // No trash bin perms, only view
+                'view-audit-logs',
+
+                // PROFILE
                 'view-profile',
                 'manage-profile',
             ],
 
             'pmo_head' => [
-                'view-inventory-scheduling',
-                'create-inventory-scheduling',
-                'update-inventory-scheduling',
-                'delete-inventory-scheduling',
-
+                // INVENTORY LIST
                 'view-inventory-list',
                 'view-own-unit-inventory-list',
                 'create-inventory-list',
                 'update-inventory-list',
                 'delete-inventory-list',
+                
+                // INVENTORY SCHEDULING
+                'view-inventory-scheduling',
+                'create-inventory-scheduling',
+                'update-inventory-scheduling',
+                'delete-inventory-scheduling',
 
+                // TRANSFERS
                 'view-transfers',
                 'create-transfers',
                 'update-transfers',
                 'delete-transfers',
 
+                // TURNOVER DISPOSAL
                 'view-turnover-disposal',
                 'create-turnover-disposal',
                 'update-turnover-disposal',
                 'delete-turnover-disposal',
 
+                // OFF CAMPUS
                 'view-off-campus',
                 'create-off-campus',
                 'update-off-campus',
                 'delete-off-campus',
                 'restore-off-campus',
 
-                'view-assignments',
-                'create-assignments',
-                'update-assignments',
-                'delete-assignments',
-
-                'view-buildings',
-                'create-buildings',
-                'update-buildings',
-                'delete-buildings',
-
-                'view-building-rooms',
-                'create-building-rooms',
-                'update-building-rooms',
-                'delete-building-rooms',
-
+                // CATEGORIES
                 'view-categories',
                 'create-categories',
                 'update-categories',
                 'delete-categories',
 
+                // EQUIPMENT CODES
+                'view-equipment-codes',
+                'create-equipment-codes',
+                'update-equipment-codes',
+                'delete-equipment-codes',
+
+                // ASSET MODELS
                 'view-asset-models',
                 'create-asset-models',
                 'update-asset-models',
                 'delete-asset-models',
+                
+                // ASSIGNMENTS
+                'view-assignments',
+                'create-assignments',
+                'update-assignments',
+                'delete-assignments',
 
+                // BUILDINGS
+                'view-buildings',
+                'create-buildings',
+                'update-buildings',
+                'delete-buildings',
+
+                // BUILDING ROOMS
+                'view-building-rooms',
+                'create-building-rooms',
+                'update-building-rooms',
+                'delete-building-rooms',
+
+                // PERSONNELS
+                'view-personnels',
+                'create-personnels',
+                'update-personnels',
+                'delete-personnels',
+
+                // UNIT OR DEPARTMENTS
                 'view-unit-or-departments',
                 'create-unit-or-departments',
                 'update-unit-or-departments',
                 'delete-unit-or-departments',
 
+                // USERS MANAGEMENT
                 'view-users-page',
                 'approve-users',
                 'reset-user-password',
                 'send-email-change-request',
                 'delete-users',
 
+                // ROLE MANAGEMENT
                 'view-roles-page',
                 'create-roles',
                 'update-roles',
                 'update-permissions',
 
+                // FORM APPROVALS
                 'view-form-approvals',
                 'approve-form-approvals',
                 'delete-form-approvals',
 
+                // SIGNATORIES
+                'view-signatories',
+                'create-signatories',
+                'update-signatories',
+                'delete-signatories',
+
+                // NOTIFICATIONS
+                'view-notifications',
+                'update-notifications',
+                'archive-notifications',
+                'delete-notifications',
+
+                // REPORTS
+                'view-reports',
+
+                // TRASH BIN
+                'view-trash-bin',
+                'restore-trash-bin',
+                'permanent-delete-trash-bin',
+
+                // AUDIT LOG
+                'view-audit-logs',
+
+                // PROFILE
                 'view-profile',
                 'manage-profile',
             ],
 
             'pmo_staff' => [
+                // INVENTORY LIST
+                'view-inventory-list',
+                'view-own-unit-inventory-list',
+                'create-inventory-list',
+                'update-inventory-list',
+                'delete-inventory-list',
+
+                // INVENTORY SCHEDULING
                 'view-inventory-scheduling',
                 'create-inventory-scheduling',
                 'update-inventory-scheduling',
                 'delete-inventory-scheduling',
 
-                'view-inventory-list',
-                'create-inventory-list',
-                'update-inventory-list',
-                'delete-inventory-list',
-
+                // TRANSFERS
                 'view-transfers',
                 'create-transfers',
                 'update-transfers',
                 'delete-transfers',
 
+                // TURNOVER DISPOSAL
                 'view-turnover-disposal',
                 'create-turnover-disposal',
                 'update-turnover-disposal',
                 'delete-turnover-disposal',
 
+                // OFF CAMPUS
                 'view-off-campus',
                 'create-off-campus',
                 'update-off-campus',
                 'delete-off-campus',
                 'restore-off-campus',
 
-                'view-reports',
-
-                'view-assignments',
-                'create-assignments',
-                'update-assignments',
-                'delete-assignments',
-
-                'view-buildings',
-                'create-buildings',
-                'update-buildings',
-                'delete-buildings',
-
-                'view-building-rooms',
-                'create-building-rooms',
-                'update-building-rooms',
-                'delete-building-rooms',
-
+                // CATEGORIES
                 'view-categories',
                 'create-categories',
                 'update-categories',
                 'delete-categories',
 
+                // EQUIPMENT CODES
+                'view-equipment-codes',
+                'create-equipment-codes',
+                'update-equipment-codes',
+                'delete-equipment-codes',
+
+                // ASSET MODELS
                 'view-asset-models',
                 'create-asset-models',
                 'update-asset-models',
                 'delete-asset-models',
 
+                // ASSIGNMENTS
+                'view-assignments',
+                'create-assignments',
+                'update-assignments',
+                'delete-assignments',
+
+                // BUILDINGS
+                'view-buildings',
+                'create-buildings',
+                'update-buildings',
+                'delete-buildings',
+
+                // BUILDING ROOMS
+                'view-building-rooms',
+                'create-building-rooms',
+                'update-building-rooms',
+                'delete-building-rooms',
+
+                // PERSONNELS
+                'view-personnels',
+                'create-personnels',
+                'update-personnels',
+                'delete-personnels',
+
+                // UNIT OR DEPARTMENTS
                 'view-unit-or-departments',
                 'create-unit-or-departments',
                 'update-unit-or-departments',
                 'delete-unit-or-departments',
 
-                'view-users-page',
+                // USERS MANAGEMENT - No user management perms
 
+                // NO ROLE MANAGEMENT
+
+                // FORM APPROVALS - No approval perms aside from view
                 'view-form-approvals',
 
+                // SIGNATORIES
+                'view-signatories',
+                'create-signatories',
+                'update-signatories',
+                'delete-signatories',
+
+                // NOTIFICATIONS
+                'view-notifications',
+                'update-notifications',
+                'archive-notifications',
+                'delete-notifications',
+
+                // REPORTS
+                'view-reports',
+
+                // TRASH BIN - No permanent delete for PMO Staff
+                'view-trash-bin',
+                'restore-trash-bin',
+
+                // AUDIT LOG
+                // 'view-audit-logs', //current unsure if staff should have access to audit logs
+
+                // PROFILE
                 'view-profile',
                 'manage-profile',
             ],
