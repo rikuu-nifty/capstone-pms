0004c4dc94e5f59a841b463aee70a76f293df368
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-10-12 15:17:29 +0800

    Profile settings: Removed unused import for DeleteUser

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -3,7 +3,7 @@ import { Transition } from '@headlessui/react';
 import { Head, Link, useForm, usePage, router } from '@inertiajs/react';
 import { FormEventHandler, useState } from 'react';
 
-import DeleteUser from '@/components/delete-user';
+// import DeleteUser from '@/components/delete-user';
 import HeadingSmall from '@/components/heading-small';
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
