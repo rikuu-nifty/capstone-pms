ac07c64742a40c6e49b811b019147c63b5a5a6ae
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-10-21 22:17:28 +0800

    Resized personnel chart in index reports

--- resources/js/pages/reports/charts/PersonnelAssignmentsChart.tsx
+++ resources/js/pages/reports/charts/PersonnelAssignmentsChart.tsx
@@ -26,7 +26,7 @@ type PersonnelAssignmentsChartProps = {
 export default function PersonnelAssignmentsChart({ data }: PersonnelAssignmentsChartProps) {
     if (!data || data.length === 0) {
         return (
-        <div className="flex h-full items-center justify-center text-sm text-gray-400">
+        <div className="flex h-full justify-center text-sm text-gray-400 min-h-[300px] w-full">
             No data available
         </div>
         );
@@ -41,7 +41,7 @@ export default function PersonnelAssignmentsChart({ data }: PersonnelAssignments
         <div className="rounded-lg bg-gray-50 p-3">
         <ChartContainer
             config={chartConfig}
-            className="mx-auto aspect-[4/3] max-h-[277px] w-full"
+            className="mx-auto aspect-[16/9] min-h-[276px] max-h-[276px] w-full"
         >
             <ResponsiveContainer width="100%" height="100%">
             <BarChart data={data}>
